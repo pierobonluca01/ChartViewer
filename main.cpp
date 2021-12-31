8acc0cb8c6c80992d285e02e6bc71d9340231768
pierobonluca01 <pierobonluca01@gmail.com>
2021-12-31 16:27:12 +0100

    Modifiche minori.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,6 @@
 using std::cout;
 using std::endl;
 
-void info(Graph* x) {
-    cout<<endl<<x->getRows()<<" "<<x->getColumns()<<" "<<x->t.size()<<"x"<<x->t[0].size()<<" "<<x->t.capacity()<<"x"<<x->t[0].capacity()<<endl;
-}
-
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
